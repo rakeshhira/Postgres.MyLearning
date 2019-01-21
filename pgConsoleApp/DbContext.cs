b799aad1a37533d6b0bfe51f8bd5be56e61619f4
Rakesh Hira <rakesh.hira@philips.com>
2019-01-21 11:05:37 -0500

    Initial checkin based upon Getting Started article of EF Core

---
+++ pgConsoleApp/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace pgConsoleApp
+{
+	public class BloggingContext : DbContext
+	{
+		public DbSet<Blog> Blogs { get; set; }
+
+		public DbSet<Post> Posts { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseNpgsql("Host=localhost;Database=my_pgConsoleApp;Username=postgres;Password=guest");
+	}
+}
