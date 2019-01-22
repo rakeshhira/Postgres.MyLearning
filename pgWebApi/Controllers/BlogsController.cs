25c7d0db069503a6170f055c929da609c30c6b93
Rakesh Hira <rakesh.hira@philips.com>
2019-01-22 08:14:39 -0500

    Based upon connection strings / resiliency article of EF Core.

---
+++ pgWebApi/Controllers/BlogsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pgWebApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace pgWebApi.Controllers
+{
+	[Route("api/[controller]")]
+	public class BlogsController : Controller
+	{
+		private readonly DbContextOptions<BloggingContext> _options;
+
+		public BlogsController(DbContextOptions<BloggingContext> options)
+		{
+			_options = options;
+		}
+
+		// GET: api/<controller>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			List<string> urls = new List<string>();
+			using (var db = new BloggingContext(_options))
+			{
+				Trace.WriteLine("All blogs in database:");
+				foreach (var blog in db.Blogs)
+				{
+					Trace.WriteLine($" - {blog.Url}");
+					urls.Add(blog.Url);
+				}
+			}
+
+			return urls;
+		}
+
+		// GET api/<controller>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<controller>
+		[HttpPost]
+		public void Post([FromBody]string url)
+		{
+			using (var db = new BloggingContext(_options))
+			{
+				db.Blogs.Add(new Blog { Url = url });
+				var count = db.SaveChanges();
+				Trace.WriteLine($"{count} records saved to database");
+				Trace.WriteLine("All blogs in database:");
+				foreach (var blog in db.Blogs)
+				{
+					Trace.WriteLine(" - {0}", blog.Url);
+				}
+			}
+		}
+
+		// PUT api/<controller>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ pgWebApi/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using pgWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace pgWebApi
+{
+	public class Startup
+	{
+		public static ILoggerFactory MyLoggerFactory = null;
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+			var connection = @"Host=localhost;Database=my_pgConsoleApp;Username=postgres;Password=guest";
+			if (MyLoggerFactory == null)
+			{
+				MyLoggerFactory = services.BuildServiceProvider().GetService<ILoggerFactory>();
+			}
+
+			services
+				.AddEntityFrameworkNpgsql()
+				.AddDbContext<BloggingContext>(dbContextOptions => dbContextOptions
+					//.UseLoggerFactory(MyLoggerFactory) 
+					.UseNpgsql(connection, connOptions => connOptions.EnableRetryOnFailure()));
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+	}
+}
